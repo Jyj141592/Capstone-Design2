db26ae12221a53de5f68ecc297d7e611efad7352
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2025-03-19 00:39:02 +0900

    Initial commit

---
+++ backend/moem/src/test/java/com/moemhub/moem/MoemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moemhub.moem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
