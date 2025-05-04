77e50368418f1a54447aff29b044a6a4d704a963
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2025-05-04 21:51:50 +0900

    update App.jsx

--- frontend/moem/src/App.jsx
+++ frontend/moem/src/App.jsx
@@ -5,6 +5,9 @@ import Register from "./pages/Register";
 import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import MyPage from "./pages/MyPage";
+import ClubLayout from "./layouts/ClubLayout";
+import ClubMain from "./pages/ClubMain";
+import ExploreClub from "./pages/ExploreClub";
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/club/" element={<ClubLayout/>}>
+            <Route path=":clubId" element={<ClubMain/>}/>
+          </Route>
           {/* 추가 페이지들은 여기에 계속 나열 */}
         </Routes>
       </BrowserRouter>
